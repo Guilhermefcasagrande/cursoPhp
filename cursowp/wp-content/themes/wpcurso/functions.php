5351dc6c5b55ceb20711eb8945806b8c0e0358e9
Guilhermefcasagrande <guilhermefranciscocasagrande@gmail.com>
2018-05-16 20:45:34 -0300

    Criada estrutura de blog

---
+++ cursowp/wp-content/themes/wpcurso/functions.php
@@ -0,0 +1,5 @@
+<?php 
+    // Habilita as imagens destacadas
+    add_theme_support('post-thumbnails');
+    // add_image_size('thumb-custom', 200, 200, true);
+?>
--- cursowp/wp-content/themes/wpcurso/index.php
+++ cursowp/wp-content/themes/wpcurso/index.php
@@ -1,3 +1,36 @@
 <?php get_header(); ?>
 
+<div class="blog">
+    <div class="wrap">
+        <h2>BLOG</h2>
+
+        <div class="box-list-posts">
+
+            <?php 
+                $args = array("post_type" => "post", "showposts" => 2);
+                $my_posts = get_posts($args);
+            ?>
+            <?php 
+                $cont = 1;
+                if($my_posts) : foreach ($my_posts as $post) : setup_postdata($post);
+            ?>
+
+            <div class="list-posts <?php if($cont == 2) echo "segundo-post" ?>">
+                <?php the_post_thumbnail(); ?>
+                
+                <div class="box-content-post">
+                    <h2><?php the_title(); ?></h2>
+                    <p>
+                        <?php the_excerpt(); ?>
+                    </p>
+                    <a href="<?php the_permalink(); ?>" class="custom-botao">Leia Mais</a>
+                </div>
+
+            </div>
+            <?php $cont ++; endforeach; endif;?>
+            
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
